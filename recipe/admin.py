343d93df57874a7a897592b98862cfe84406d1b3
emir-muradosilov <emir.muradosilov@yandex.ru>
2024-10-05 15:33:40 +0300

    пофиксил баги с сохранением и отображением изображений. добавил новую модель - категории. связанное поле в таблице recept сategory и таблице category по полю id

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import Recept
-
+from .models import Category
 # Register your models here.
 
 class ReceptStyle(admin.ModelAdmin):
-    list_display = ['id','title','date', 'author']
+    list_display = ['id','title','date', 'author', 'is_published']
     list_display_links = ['id','title',]
     readonly_fields = ('date',)
     list_filter = ('date','author',)
+    list_editable = ('is_published',)
+    readonly_fields = ('watched',)
 
 
 admin.site.register(Recept, ReceptStyle)
+admin.site.register(Category)
